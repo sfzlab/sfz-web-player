ce5a6e379750218f0147ad74d037133638560799
Kim T <kimturley@gmail.com>
2023-04-08 15:35:06 -0700

    Parse by character

--- src/utils/parser.ts
+++ src/utils/parser.ts
@@ -2,64 +2,64 @@ import { FileLocal, FileRemote } from "../types/files";
 import FileLoader from "./fileLoader";
 
 let loader: FileLoader;
-let header: string;
 
-async function processToken(token: string, map: any, values: any) {
-  console.log("processToken", token);
-  if (token.startsWith("<")) {
-    header = token.slice(1, -1);
-    values = {};
-    if (!map[header]) map[header] = [];
-    map[header].push(values);
-  }
-  if (token.startsWith("#")) {
-    const file: FileLocal | FileRemote | undefined = await loader.getFile(
-      loader.root + token.slice(10, -1)
-    );
-    const include: any = await parseSfz(file?.contents);
-    console.log(loader.root + token.slice(10, -1), header, include);
-    map[header].push(include);
-  }
-  if (token.includes("=")) {
-    const [id, val] = token.split("=");
-    if (!isNaN(val as any)) {
-      values[id] = Number(val);
-    } else {
-      values[id] = val;
-    }
-  }
-}
+const skipCharacters: string[] = [" ", "\t", "\r", "\n"];
+const endCharacters: string[] = ["\r", "\n"];
 
-function getLines(str: string) {
-  return str
-    .replace(
-      /(?:(?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:(?<!\:|\\\|\')\/\/.*))/gm,
-      ""
-    )
-    .split(/\r?\n/);
+async function loadSfz(path: string) {
+  const file: FileLocal | FileRemote | undefined = await loader.getFile(
+    loader.root + path
+  );
+  return await parseSfz(file?.contents);
 }
 
 async function parseSfz(contents: string) {
+  let header: string = "";
   const map: any = {};
-  const values: any = {};
-  const lines: string[] = getLines(contents);
-  for (let line of lines) {
-    line = line.trim();
-    if (line.length === 0 || line.startsWith("//")) continue;
-    if (line.includes(" ") && !line.startsWith("#")) {
-      const sublines: string[] = line.split(/ /);
-      sublines.forEach(
-        async (subline) => await processToken(subline, map, values)
-      );
+  let values: any = {};
+  for (let i: number = 0; i < contents.length; i++) {
+    const char: string = contents.charAt(i);
+    if (skipCharacters.includes(char)) continue;
+    const iEnd: number = findEnd(contents, i);
+    if (char === "/") {
+      // const comment: string = contents.slice(i + 2, iEnd);
+      // console.log("comment", comment);
+    } else if (char === "#") {
+      const directive: string = contents.slice(i + 10, iEnd - 1);
+      const directiveValues: any = await loadSfz(directive);
+      console.log("directive", directive);
+      console.log("directiveValues", directiveValues);
+    } else if (char === "<") {
+      header = contents.slice(i + 1, iEnd - 1);
+      values = {};
+      if (!map[header]) map[header] = [];
+      map[header].push(values);
+      console.log("header", header);
     } else {
-      await processToken(line, map, values);
+      const opcode: string = contents.slice(i, iEnd);
+      const [opcodeName, opcodeValue]: any[] = opcode.split("=");
+      let opcodeValueTyped: any = opcodeValue;
+      if (!isNaN(opcodeValue as any)) {
+        opcodeValueTyped = Number(opcodeValue);
+      }
+      values[opcodeName] = opcodeValueTyped;
+      console.log("opcode", opcodeName, opcodeValueTyped);
     }
+    i = iEnd;
   }
   return map;
+}
+
+function findEnd(contents: string, startAt: number) {
+  for (let index: number = startAt; index < contents.length; index++) {
+    const char: string = contents.charAt(index);
+    if (endCharacters.includes(char)) return index;
+  }
+  return contents.length;
 }
 
 function setLoader(fileLoader: FileLoader) {
   loader = fileLoader;
 }
 
-export { parseSfz, setLoader };
+export { loadSfz, parseSfz, setLoader };
