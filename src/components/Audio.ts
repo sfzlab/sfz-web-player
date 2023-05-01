641a87d217f16f573fadd1ad68447669b2b0d851
Kim T <kimturley@gmail.com>
2023-04-30 17:03:11 -0700

    Prototype loading audio samples

--- src/components/Audio.ts
+++ src/components/Audio.ts
@@ -9,10 +9,13 @@ class Audio extends Event {
   loader: FileLoader;
   private synth: AudioTinySynth;
   private audio: AudioContext;
+  private audioBuffer: AudioBufferSourceNode;
+  private samples: string[] = [];
 
   constructor(options: AudioOptions) {
     super();
     this.audio = new AudioContext();
+    this.audioBuffer = this.audio.createBufferSource();
     if (!window.WebAudioTinySynth) {
       window.alert("webaudio-tinysynth not found, add to a <script> tag.");
     }
@@ -36,12 +39,10 @@ class Audio extends Event {
       );
       this.showFile(file);
     }
-    this.loadSample(
-      "https://raw.githubusercontent.com/freepats/glass/main/samples/C5_01.flac"
-    );
   }
 
   async loadSample(url: string) {
+    console.log("loadSample", url);
     const file: FileLocal | FileRemote | undefined = this.loader.addFile(url);
     console.log("file", file);
     const newFile = await this.loader.getFile(file, true);
@@ -50,8 +51,24 @@ class Audio extends Event {
 
   async showFile(file: FileLocal | FileRemote | undefined) {
     file = await this.loader.getFile(file);
+    const sfzObject: any = await parseSfz(file?.contents);
     console.log("showFile", file);
-    console.log("parseSFZ", await parseSfz(file?.contents));
+    console.log("parseSFZ", sfzObject);
+
+    // hardcoded prototype for one sfz file
+    if (sfzObject.master) {
+      sfzObject.master[0].region.forEach((region: any) => {
+        const samplePath: string = this.loader.root.replace(
+          "Programs/",
+          region.sample.replace("../", "")
+        );
+        this.samples[region.lokey] = samplePath;
+      });
+      this.samples.forEach(
+        async (sample: string) => await this.loadSample(sample)
+      );
+      console.log(this.samples);
+    }
   }
 
   onKeyboard(event: any) {
@@ -64,8 +81,23 @@ class Audio extends Event {
     this.dispatchEvent("change", controlEvent);
   }
 
-  setSynth(event: AudioControlEvent) {
-    this.synth.send([event.channel, event.note, event.velocity]);
+  async setSynth(event: AudioControlEvent) {
+    // Demo using AudioTinySynth
+    // this.synth.send([event.channel, event.note, event.velocity]);
+
+    // prototype using samples
+    console.log("setSynth", event);
+    if (event.velocity === 0) {
+      this.audioBuffer.stop();
+      return;
+    }
+    const samplePath: string = this.samples[event.note];
+    console.log("samplePath", event.note, samplePath);
+    const newFile = await this.loader.getFile(samplePath, true);
+    this.audioBuffer = this.audio.createBufferSource();
+    this.audioBuffer.buffer = newFile?.contents;
+    this.audioBuffer.connect(this.audio.destination);
+    this.audioBuffer.start(0);
   }
 }
 
