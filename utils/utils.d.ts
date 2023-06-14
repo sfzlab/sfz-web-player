d4c779d593df17f43e39e44a551308a59b94a51c
kmturley <kmturley@users.noreply.github.com>
2023-06-14 05:07:45 +0000

    deploy: fd187aeb170af7107d5c5fe900fd8b81e9760f21

--- utils/utils.d.ts
+++ utils/utils.d.ts
@@ -1,7 +1,8 @@
 declare function encodeHashes(item: string): string;
+declare function midiNameToNum(name: string | number): any;
 declare function pathDir(item: string, separator?: string): string;
 declare function pathExt(item: string): string;
 declare function pathJoin(...segments: any): string;
 declare function pathRoot(item: string, separator?: string): string;
 declare function pathSubDir(item: string, dir: string): string;
-export { encodeHashes, pathDir, pathExt, pathJoin, pathRoot, pathSubDir };
+export { encodeHashes, midiNameToNum, pathDir, pathExt, pathJoin, pathRoot, pathSubDir };
