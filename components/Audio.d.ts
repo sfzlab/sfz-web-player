aaaeffe1462967fd0949f603341771f39bada4c7
kmturley <kmturley@users.noreply.github.com>
2024-02-06 06:06:46 +0000

    deploy: dbf4999d61c9c5c0f4af23a960bcda411720714f

--- components/Audio.d.ts
+++ components/Audio.d.ts
@@ -3,7 +3,7 @@ import { AudioOptions } from '../types/player';
 import Event from './event';
 import { FileLocal, FileRemote } from '../types/files';
 import FileLoader from '../utils/fileLoader';
-import { ParseHeader, ParseOpcodeObj } from '@sfz-tools/core/dist/types/parse';
+import { ParseOpcodeObj } from '@sfz-tools/core/dist/types/parse';
 declare class Audio extends Event {
     loader: FileLoader;
     private regions;
@@ -15,7 +15,6 @@ declare class Audio extends Event {
     private regionDefaults;
     constructor(options: AudioOptions);
     showFile(file: FileLocal | FileRemote | undefined): Promise<void>;
-    fixPaths(headers: ParseHeader[], file: FileLocal | FileRemote): void;
     getKeyboardMap(regions: ParseOpcodeObj[]): AudioKeyboardMap;
     preloadFiles(regions: ParseOpcodeObj[]): Promise<void>;
     checkRegion(region: ParseOpcodeObj, controlEvent: AudioControlEvent, rand: number): boolean;
